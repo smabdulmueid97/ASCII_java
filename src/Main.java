7bd2bee5831324b0c4e44a88374560c88a6c02a7
smabdulmueid97 <73439037+smabdulmueid97@users.noreply.github.com>
2023-01-05 02:51:16 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        int one='1';
+        int capitalA ='A';
+        int capitalB ='B';
+        int smallA ='a';
+        int smallB = 'b';
+        System.out.println("Question 1.a):");
+        System.out.println("1 = "+one);
+        System.out.println("A = "+capitalA);
+        System.out.println("B = "+capitalB);
+        System.out.println("a = "+smallA);
+        System.out.println("b = "+smallB);
+        System.out.println("");
+
+        char a= (char)40;
+        char b= (char)59;
+        char c= (char)79;
+        char d= (char)85;
+        char e= (char)90;
+        System.out.println("Question 1.b):");
+        System.out.println("40 = "+a);
+        System.out.println("59 = "+b);
+        System.out.println("79 = "+c);
+        System.out.println("85 = "+d);
+        System.out.println("90 = "+e);
+        System.out.println("");
+
+    }
+}
